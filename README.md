5a66f85790423e59855c7482ac127afc27420360
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-17 12:56:17 +0530

    Urllib Basic

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # core_python_pluralsight
+
+
+# Basic
+
+- When using urllib data is returned as bytes, which can be decoded into string by decode method
---
+++ basic.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+
+story = urlopen('http://sixty-north.com/c/t.txt')
+
+story_words_decoded = []
+story_words = []
+
+for line in story:
+    line_words_decoded = line.decode('utf-8').split()
+    line_words = line.split()
+    for word in line_words:
+        story_words.append(word)
+    for word in line_words_decoded:
+        story_words_decoded.append(word)
+
+
+story.close()
+print(story_words)
+print(story_words_decoded)
