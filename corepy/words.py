aeed077243f19c41efac7e1e3bf2789c78c64440
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-18 19:37:26 +0530

    Dunder

--- corepy/words.py
+++ corepy/words.py
@@ -1,18 +1,24 @@
 from urllib.request import urlopen
 
-story = urlopen('http://sixty-north.com/c/t.txt')
 
-story_words_decoded = []
-story_words = []
+def fetch_words():
+    story = urlopen('http://sixty-north.com/c/t.txt')
 
-for line in story:
-    line_words_decoded = line.decode('utf-8').split()
-    line_words = line.split()
-    for word in line_words:
-        story_words.append(word)
-    for word in line_words_decoded:
-        story_words_decoded.append(word)
-story.close()
+    story_words_decoded = []
+    story_words = []
 
-print(story_words)
-print(story_words_decoded)
+    for line in story:
+        line_words_decoded = line.decode('utf-8').split()
+        line_words = line.split()
+        for word in line_words:
+            story_words.append(word)
+        for word in line_words_decoded:
+            story_words_decoded.append(word)
+    story.close()
+
+    print(story_words)
+    print(story_words_decoded)
+
+
+if __name__ == '__main__':
+    fetch_words()
